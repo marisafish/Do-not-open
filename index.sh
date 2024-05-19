5f070ace7c53cb7b2db0d8d90e8147f63b9199c0
marisafish <156426352+marisafish@users.noreply.github.com>
2024-05-19 19:05:01 +0900

    Update index.sh

--- index.sh
+++ index.sh
@@ -3,6 +3,15 @@ set fileencodings=utf-8,ucs-bom,iso-2022-jp-3,ucs-2le,ucs-2,cp932
 
 while true
 do
-  echo LOOP
+  echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
   zenity --error --title="オシャレース" --text="おせちんこwww" 2>/dev/null
+  sleep 1
+  zenity --warning --title="やめろ" --text="PCやりすぎ">/dev/null
+  sleep 1
+  zenity --info --title="カウントダウン" --text="3>/dev/null
+  sleep 1
+  zenity --info --title="カウントダウン" --text="2">/dev/null
+  sleep 1
+  zenity --info --title="カウントダウン" --text="1">/dev/null
 done
+zenity --question --title="終わるのか" --text="yesかnoで終わり">/dev/null
